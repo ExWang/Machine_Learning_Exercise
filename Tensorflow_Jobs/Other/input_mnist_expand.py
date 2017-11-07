cad6ce083605b87c576e3c9ed873f7349df70629
ExWang <wangshihao1117@hotmail.com>
2017-11-07 21:16:00 +0800

    Update input_mnist_expand.py
    Add the batch function

--- Tensorflow_Jobs/Other/input_mnist_expand.py
+++ Tensorflow_Jobs/Other/input_mnist_expand.py
@@ -1,10 +1,8 @@
-
 import os
 import numpy
 import tensorflow as tf
 import numpy as np
 from PIL import Image
-
 
 import matplotlib.pyplot as plt
 
@@ -27,6 +25,7 @@ test_labels = 't10k-labels.idx1-ubyte'
 
 def _int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
 
 def _bytes_feature(value):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
@@ -55,9 +54,9 @@ def load_mnist(isTest):
 
 def trans2tfrecord(isTest, isShowSimple):
     TFRname = ''
-    if isTest==False:
+    if isTest == False:
         TFRname = 'train_' + str(resize_height) + 'x' + str(resize_width) + '.tfrecords'
-    if isTest==True:
+    if isTest == True:
         TFRname = 'test_' + str(resize_height) + 'x' + str(resize_width) + '.tfrecords'
     print TFRname
     writer = tf.python_io.TFRecordWriter(TFRname)
@@ -92,15 +91,16 @@ def trans2tfrecord(isTest, isShowSimple):
 
     writer.close()
 
-def trans2original(isTest, isShowSimple):
+
+def trans2original(isTest, isShowSimple, num_epochs):
     reader = tf.TFRecordReader()
     TFRname = ''
-    if isTest== False:
+    if isTest == False:
         TFRname = 'train_' + str(resize_height) + 'x' + str(resize_width) + '.tfrecords'
-    if isTest==True:
+    if isTest == True:
         TFRname = 'test_' + str(resize_height) + 'x' + str(resize_width) + '.tfrecords'
     print TFRname
-    filename_queue = tf.train.string_input_producer([TFRname])
+    filename_queue = tf.train.string_input_producer([TFRname], num_epochs=num_epochs)
 
     # one simple
     _, serialized_example = reader.read(filename_queue)
@@ -113,6 +113,9 @@ def trans2original(isTest, isShowSimple):
             'label': tf.FixedLenFeature([], tf.int64),
         })
     images = tf.decode_raw(features['image_raw'], tf.uint8)
+    images = tf.reshape(images, [224, 224])
+    images = tf.cast(images, tf.float32) * (1. / 255) - 0.5
+
     labels = tf.cast(features['label'], tf.int32)
 
     if isShowSimple:
@@ -133,11 +136,31 @@ def trans2original(isTest, isShowSimple):
     return images, labels
 
 
+def input_mnist_batch(data_set, batch_size, num_epochs):
+    if not num_epochs:
+        num_epochs = None
+    if data_set == 'train':
+        isTest = False
+    else:
+        isTest = True
+
+    # load from my func
+    image, label = trans2original(isTest=isTest, isShowSimple=False, num_epochs=num_epochs)
+
+    images, labels = tf.train.shuffle_batch([image, label],
+                                            batch_size=batch_size,
+                                            num_threads=64,
+                                            capacity=1000+3*batch_size,
+                                            min_after_dequeue=1000
+                                            )
+
+    return images, labels
+
+
 if __name__ == '__main__':
     isTest = True
-    #trans2tfrecord(isTest=isTest, isShowSimple=False)
-    print 'TFRecords transformed complete!'
-    trans2original(isTest=isTest, isShowSimple=True)
-
-
-
+    # trans2tfrecord(isTest=isTest, isShowSimple=False)
+    # print 'TFRecords transformed complete!'
+    # trans2original(isTest=isTest, isShowSimple=True)
+    input_mnist_batch(data_set='train', batch_size=32, num_epochs=None)
+    print 'Works done!'
